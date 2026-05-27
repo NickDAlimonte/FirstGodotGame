460a4cd01ba7f69b656ea00d428c32ffbd55d9d4
Nickolas D'Alimonte <ND135@myscc.ca>
2026-05-27 12:21:55 -0400

    Created a main menu scene with a play button which changes the scene to the gameworld which is where the game is located now

---
+++ game-project/scripts/PlayGame.gd
@@ -0,0 +1,14 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pressed.connect(_on_button_pressed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_button_pressed():
+	var game_scene = preload("res://GameWorld.tscn")
+	game_scene.instantiate()
+	
+	get_tree().change_scene_to_file("res://GameWorld.tscn")
